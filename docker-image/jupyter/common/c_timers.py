ed8ad6cddc08173fc17bb8d35b27efa7201911fe
Piotr Aksamit <42714030+zpaks@users.noreply.github.com>
2024-05-12 23:25:44 +0200

    Update c_timers.py

--- docker-image/jupyter/common/c_timers.py
+++ docker-image/jupyter/common/c_timers.py
@@ -32,30 +32,75 @@ import numpy
 start = numpy.repeat(0.0, 64)
 elapsed = numpy.repeat(0.0, 64)
 
+energy_start = numpy.repeat(0.0, 64)
+energy_used = numpy.repeat(0.0, 64)
+
+
+################################# -- D -- ###################################
+
+from random import randrange
+cpu_zone = '/sys/devices/virtual/powercap/intel-rapl/intel-rapl:0'
+
+
+def energy_uj():
+	# return randrange(300)
+	# TODO verify
+	with open(f"{cpu_zone}/energy_uj") as energy_file:
+		return int(energy_file.readline())
+
+
+def max_energy():
+	# return 300
+	# TODO verify
+	with open(f"{cpu_zone}/max_energy_range_uj") as file:
+		return int(file.readline())
+
+def energy_consumption(energy_uj_start, energy_uj_end):
+	# TODO verify
+	# return randrange(300)
+	return energy_uj_end - energy_uj_start if energy_uj_end > energy_uj_start else max_energy() - energy_uj_start + energy_uj_end
+
+################################# -- D -- ###################################
+
+
+
+
 #*****************************************************************
 #******            T  I  M  E  R  _  C  L  E  A  R          ******
 #*****************************************************************
 def timer_clear(n):
 	global elapsed
+	global energy_used
 	elapsed[n] = 0.0
+	energy_used[n] = 0.0
 
 #*****************************************************************
 #******            T  I  M  E  R  _  S  T  A  R  T          ******
 #*****************************************************************
 def timer_start(n):
 	global start
+	global energy_start
 	start[n] = time.time()
+	energy_start[n] = energy_uj()
 
 #*****************************************************************
 #******            T  I  M  E  R  _  S  T  O  P             ******
 #*****************************************************************
 def timer_stop(n):
 	global elapsed
+	global energy_used
 	t = (time.time() - start[n])
 	elapsed[n] += t
+	e = energy_consumption(energy_start[n], energy_uj())
+	energy_used[n] += e
 
 #*****************************************************************
 #******            T  I  M  E  R  _  R  E  A  D             ******
 #*****************************************************************
 def timer_read(n):
 	return elapsed[n]
+
+
+def energy_read(n):
+	return energy_used[n]
+
